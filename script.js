2ee8621c65bd87fe2ff43be7e2ff05e56e4ed1f0
Ayaan Khalif <ayaankhalif@Ayaans-MacBook-Air-4.local>
2023-06-21 13:47:25 +0300

    broken-keys assignment - Done

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-// Good Luck 💪🏾
+
+
+function brokenKeys(rightPhrase, typedPhrase) {
+    var brokenKeys = [];
+  
+    for (var i = 0; i < rightPhrase.length; i++) {
+      var rightChar = rightPhrase.charAt(i);
+      var typedChar = typedPhrase.charAt(i);
+  
+      if (rightChar !== typedChar && brokenKeys.indexOf(rightChar) === -1) {
+        brokenKeys.push(rightChar);
+      }
+    }
+  
+    return brokenKeys;
+  }
+  
+  console.log(brokenKeys("Subax wanagsan", "Subah wanagsan")); 
+ 
